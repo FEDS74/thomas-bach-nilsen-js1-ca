ca74af05316eafb3865272ac1be7bf6d17ee4d15
Thomas Bach-Nilsen <55656461+FEDS74@users.noreply.github.com>
2020-03-26 08:06:16 +0100

    thursday

--- js/details.js
+++ js/details.js
@@ -1,1 +1,10 @@
+const queryString = document.location.search;
+const params = new URLSearchParams(queryString);
 
+/*let id;
+
+if (params.has("id")) {
+  id = params.get("id");
+} else {
+  document.location.href = "/";
+}*/
--- js/script.js
+++ js/script.js
@@ -1,19 +1,10 @@
 const queryString = document.location.search;
 const params = new URLSearchParams(queryString);
 
-let id;
-
-if (params.has("id")) {
-  id = params.get("id");
-} else {
-  document.location.href = "/";
-}
-
-
 
 const characterUrl = "https://rickandmortyapi.com/api/character/";
 const corsEnabledUrl = "https://cors-anywhere.herokuapp.com/" + characterUrl;
-const characterIdUrl = "${corsEnabledUrl}${id}";
+
 
 
 fetch(corsEnabledUrl)
@@ -23,18 +14,19 @@ fetch(corsEnabledUrl)
   .then(function(json) {
     loopAPI(json);
   })
-  .catch(function() {
+  .catch(function(error) {
     //document.location.href = "error.html";
-    //console.log(error);
+    console.log(error);
   });
 
+  function loadResultsApi(resultsObject) {
+    const resultsArray = resultsObject.results;
 
-function loopAPI(resultsObject) {
-  const resultsArray = resultsObject.results;
+      const resultsContainer = document.querySelector(".results");
 
-  let newHTML = "";
+    let newHTML = "";
 
-  resultsArray.forEach(function(results) {
+    resultsArray.forEach(function(results) {
       console.log(results);
 
       let typeResult = "Unknown";
@@ -48,23 +40,24 @@ function loopAPI(resultsObject) {
 
       if (results.episode) {
         episodeCount = results.episode.length;
-        console.dir(results.episode.length);
+        //console.dir(results.episode.length);
       }
 
 
-        newHTML += `<div class="col-sm-6 col-md-4 col-lg-3">
-    <div class="card">
-        <img class="image" src="${results.image}" alt=${results.name}>
-        <div class="details">
-            <h4 class="name">${results.name}</h4>
-            <p>Type: ${typeResult}</p>
-            <p>Episode count: ${episodeCount}</p>
-            <a class="btn btn-primary" href="details.html?id=${characterIdUrl[i].id}">Details</a>
-        </div>
-    </div>
-</div>`;
-      });
+      newHTML += `<div class="col-sm-6 col-md-4 col-lg-3">
+      <div class="card">
+          <img class="image" src="${results.image}" alt=${results.name}>
+          <div class="details">
+              <h4 class="name">${results.name}</h4>
+              <p>Type: ${typeResult}</p>
+              <p>Episode count: ${episodeCount}</p>
+              <a class="btn btn-primary" href="details.html?id=">Details</a>
+          </div>
+      </div>
+  </div>`;
+    });
 
-    const resultsContainer = document.querySelector(".row.results"); resultsContainer.innerHTML = newHTML;
+
+    resultsContainer.innerHTML = newHTML;
 
   }
